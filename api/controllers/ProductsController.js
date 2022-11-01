1d01e4af82d17f15d52cbeb5a61d9cf1a3e35665
NguyenDangChien2001 <nguyendangchien10012001@gmail.com>
2022-11-01 14:34:01 +0700

    test3

--- api/controllers/ProductsController.js
+++ api/controllers/ProductsController.js
@@ -7,9 +7,24 @@ const db = require("./../db");
 const table = "products";
 
 module.exports = {
-  get: (req, res) => {
+  getAllMovie: (req, res) => {
     let sql = "SELECT * FROM movie";
     db.query(sql, (err, response) => {
+      if (err) throw err;
+      res.json(response);
+    });
+  },
+  getMovieResult: (req, res) => {
+    let sql = "SELECT * FROM movie where title like '%' ? '%'";
+    db.query(sql, [req.params.title], (err, response) => {
+      if (err) throw err;
+      res.json(response);
+    });
+  },
+  getMovieType: (req, res) => {
+    let sql =
+      "SELECT * FROM movie left join movie_type on movie.id = movie_type.idMovie where idType=?";
+    db.query(sql, [req.params.id], (err, response) => {
       if (err) throw err;
       res.json(response);
     });
@@ -23,6 +38,41 @@ module.exports = {
   },
   getUser: (req, res) => {
     let sql = "SELECT * FROM user";
+    db.query(sql, (err, response) => {
+      if (err) throw err;
+      res.json(response);
+    });
+  },
+  getUserLoggin: (req, res) => {
+    let sql = "SELECT * FROM user WHERE id = ?";
+    db.query(sql, [req.params.id], (err, response) => {
+      if (err) throw err;
+      res.json(response);
+    });
+  },
+  getUserPhoneLoggin: (req, res) => {
+    let sql = "SELECT * FROM user WHERE PhoneNumber = ?";
+    db.query(sql, [req.params.phone], (err, response) => {
+      if (err) throw err;
+      res.json(response);
+    });
+  },
+  getEpisode: (req, res) => {
+    let sql = "SELECT * FROM episode WHERE idMovie = ? and tap = ?";
+    db.query(sql, [req.params.id, req.params.tap], (err, response) => {
+      if (err) throw err;
+      res.json(response);
+    });
+  },
+  getAllEpisode: (req, res) => {
+    let sql = "SELECT * FROM episode where idMovie = ?";
+    db.query(sql, [req.params.id], (err, response) => {
+      if (err) throw err;
+      res.json(response);
+    });
+  },
+  getType: (req, res) => {
+    let sql = "SELECT * FROM type";
     db.query(sql, (err, response) => {
       if (err) throw err;
       res.json(response);
--- api/routes.js
+++ api/routes.js
@@ -3,15 +3,31 @@ module.exports = function (app) {
   var productsCtrl = require("./controllers/ProductsController");
 
   // todoList Routes
-  app.route("/movie").get(productsCtrl.get).post(productsCtrl.store);
+  app.route("/movie").get(productsCtrl.getAllMovie).post(productsCtrl.store);
 
-  app
-    .route("/movie/:id")
-    .get(productsCtrl.detail)
-    .put(productsCtrl.update)
-    .delete(productsCtrl.delete);
+  app.route("/movie/:id").get(productsCtrl.getMovieType);
+
+  app.route("/movie/result/:title").get(productsCtrl.getMovieResult);
+
+  // app
+  //   .route("/movie/:id")
+  //   .get(productsCtrl.detail)
+  //   .put(productsCtrl.update)
+  //   .delete(productsCtrl.delete);
 
   app.route("/random").get(productsCtrl.getRandomMovie);
 
   app.route("/user").get(productsCtrl.getUser).post(productsCtrl.storeUser);
+
+  app.route("/type").get(productsCtrl.getType);
+
+  app.route("/user/:id").get(productsCtrl.getUserLoggin);
+
+  app.route("/phone/:phone").get(productsCtrl.getUserPhoneLoggin);
+
+  app.route("/phone").get(productsCtrl.getUser);
+
+  app.route("/episode/:id/:tap").get(productsCtrl.getEpisode);
+
+  app.route("/episode/:id").get(productsCtrl.getAllEpisode);
 };
